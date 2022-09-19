610719e4d3a1e7f36bd88d71de5487350c755352
maksimpancuk <maksim.panchuk29@gmail.com>
2022-09-19 18:45:51 +0300

    Сломанный проект 1

--- src/main/java/com/panchuk/shooter/business/model/Point.java
+++ src/main/java/com/panchuk/shooter/business/model/Point.java
@@ -12,19 +12,7 @@ import java.io.Serializable;
 @Builder
 
 @Getter
-@Setter
-@ToString
-public class Point implements Serializable {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.SEQUENCE)
-//    @Column(nullable = true, name = "id")
-    @Id
-    @Column(name="id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @Column(nullable = false, name = "x")
-    private Double x;
-    @Column(nullable = false, name = "y")
+
     private Double y;
     @Column(nullable = false, name = "r")
     private Double r;
