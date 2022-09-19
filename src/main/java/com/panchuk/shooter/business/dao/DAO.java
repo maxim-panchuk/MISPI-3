3e3345abad99221820a3fa7f5e4de9c3dff37880
maksimpancuk <maksim.panchuk29@gmail.com>
2022-09-19 18:56:39 +0300

    Сломанный проект 3

--- src/main/java/com/panchuk/shooter/business/dao/DAO.java
+++ src/main/java/com/panchuk/shooter/business/dao/DAO.java
@@ -3,9 +3,7 @@ package com.panchuk.shooter.business.dao;
 import com.panchuk.shooter.business.model.Point;
 
 import java.util.List;
-
-public interface DAO {
-    void add(Point entity);
+ty);
     List<Point> getAll();
     void clear();
 }
